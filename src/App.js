c7a5d953a4330d1061ac849c1c5fc9b5d4f30208
sakshi <panchalsakshi01@gmail.com>
2023-08-04 16:45:32 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-   
+      <Home></Home>
     </div>
   );
 }
